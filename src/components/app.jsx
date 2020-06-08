bbd7ff26cb9a606571c4b8f0d4855655d1e4be43
Todd Shinto <todd_shinto@yahoo.com>
2020-06-08 23:29:42 +0000

    app render method

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import ViewCards from './view-cards';
+import ReviewCards from './review-cards';
+import CreateCard from './create-card';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'view-cards'
+    };
+  }
+
+  setView(view) {
+    this.setState({ view: view });
+  }
+
+  getView() {
+    switch (this.state.view) {
+      case 'create-card':
+        return <CreateCard />;
+      case 'review-cards':
+        return <ReviewCards />;
+      case 'view-cards':
+        return <ViewCards />;
+      default:
+        return null;
+    }
+  }
+
   render() {
-    return <h1 className="text-center">Flash Card App</h1>;
+    return (
+      <>
+        <h1 className="text-center">Flash Card App</h1>
+        <div>
+          {this.getView()}
+        </div>
+      </>
+    );
   }
 }
 
