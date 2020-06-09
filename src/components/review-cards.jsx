b234e407b3eacfff3dd6458d11ff474419a5d6cb
Todd Shinto <todd_shinto@yahoo.com>
2020-06-09 21:27:48 +0000

    next card method

--- src/components/review-cards.jsx
+++ src/components/review-cards.jsx
@@ -5,13 +5,22 @@ export default class ReviewCards extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentCard: 0,
       side: 'front'
     };
     this.componentDidMount = this.componentDidMount.bind(this);
   }
 
   componentDidMount() {
-    this.context.setActiveCard(0);
+    this.context.setActiveCard(this.state.currentCard);
+  }
+
+  nextCard() {
+    if (this.state.currentCard === this.context.cards.length - 1) {
+      return this.setState({ currentCard: 0 });
+    } else {
+      return this.setState({ currentCard: this.state.currentCard + 1 });
+    }
   }
 
   render() {
